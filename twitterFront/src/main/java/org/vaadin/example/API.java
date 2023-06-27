86544b10789327f9166bdeba0c41fbb3a6d0ccc7
Andres Arevalo <9005829@alumnos.ufv.es>
2023-06-27 22:35:28 +0200

    Front terminado

--- twitterFront/src/main/java/org/vaadin/example/API.java
+++ twitterFront/src/main/java/org/vaadin/example/API.java
@@ -1,5 +1,9 @@
 package org.vaadin.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -26,9 +30,11 @@ public class API {
     public String postTweet(Tweet tweet) throws URISyntaxException, IOException,
             InterruptedException {
         String fullUrl = String.format(urlPrefix, "tweet","");
+        Gson gson = new GsonBuilder().setDateFormat("dd-MM-yyyy").create();
+        JsonObject jsonObject = gson.toJsonTree(tweet).getAsJsonObject();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(fullUrl))
-                .POST(HttpRequest.BodyPublishers.ofString(tweet.toString()))
+                .POST(HttpRequest.BodyPublishers.ofString(jsonObject.toString()))
                 .build();
         HttpResponse<String> response = HttpClient
                 .newBuilder()
@@ -37,5 +43,19 @@ public class API {
         System.out.println(response.body());
         return response.body();
     }
-
+    // Eliminar tweet
+    public String deleteTweet(int id) throws URISyntaxException, IOException,
+            InterruptedException {
+        String fullUrl = String.format(urlPrefix, "deletetweet",id);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(fullUrl))
+                .DELETE()
+                .build();
+        HttpResponse<String> response = HttpClient
+                .newBuilder()
+                .build()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        return response.body();
+    }
 }
--- twitterFront/src/main/java/org/vaadin/example/MainView.java
+++ twitterFront/src/main/java/org/vaadin/example/MainView.java
@@ -1,5 +1,7 @@
 package org.vaadin.example;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.grid.Grid;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.flow.component.Key;
@@ -10,6 +12,9 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * A sample Vaadin view class.
@@ -37,29 +42,43 @@ public class MainView extends VerticalLayout {
      *            The message service. Automatically injected Spring managed
      *            bean.
      */
-    public MainView(@Autowired GreetService service) {
+    public MainView(@Autowired TwitterService service) {
+        VerticalLayout cabezera = new VerticalLayout();
+        VerticalLayout cuerpo = new VerticalLayout();
+        //Se crea un formulario para añadir un nuevo tweet
+        TextField datos = new TextField("Tweet");
+        datos.addThemeName("bordered");
+        Button boton1 = new Button("Añadir tweet",
+                e -> {
+                    Tweet tweet = new Tweet();
+                    tweet.setNombre("Pepe");
+                    tweet.setMensaje(datos.getValue());
+                    //Se añade con la fecha actual
+                    tweet.setFecha(new Date());
+                    datos.clear();
+                    try {
+                        service.postTweet(tweet);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    Notification.show("Tweet añadido");
+                    //Se refresca la página para que se vea el nuevo tweet
+                    UI.getCurrent().getPage().reload();
+                });
+      ArrayList<Tweet> listaTweets = new ArrayList<>();
+      try {
+          listaTweets = service.leeTweets();
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+      cabezera.add(datos, boton1);
+      //Bucle que añade todos los tweets al layout cuerpo
+        for (Tweet tweet : listaTweets) {
+            cuerpo.add(tweet.getTweetLayout());
+        }
 
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-        textField.addThemeName("bordered");
+        add(cabezera, cuerpo);
 
-        // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello",
-                e -> Notification.show(service.greet(textField.getValue())));
-
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button has a more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
-
-        // Use custom CSS classes to apply styling. This is defined in
-        // shared-styles.css.
-        addClassName("centered-content");
-
-        add(textField, button);
     }
 
 }
