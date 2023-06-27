cb260467867578434f32bb1a8a02353e7ed1555a
Andres Arevalo <9005829@alumnos.ufv.es>
2023-06-27 13:54:11 +0200

    Desarrollada lectura de JSON y metodos Get y Post

---
+++ twitterAPI/src/main/java/dis/ufv/twitterAPI/twitterAPI/LectorJSON.java
@@ -0,0 +1,38 @@
+package dis.ufv.twitterAPI.twitterAPI;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class LectorJSON {
+    public ArrayList<Tweet> LeerJSON(String fichero){
+        try {
+            FileReader reader = new FileReader(fichero);
+            ArrayList<Tweet> listaTweets = new Gson().fromJson(reader, new TypeToken<ArrayList<Tweet>>(){}.getType());
+            System.out.println("Lista de tweets cargada");
+            return listaTweets;
+
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Error al leer el fichero JSON");
+            return null;
+        }
+    }
+    //Metodo que escribe en un fichero JSON y si el JSON no existe lo crea
+    public void escribirJSON(String fichero, ArrayList<Tweet> listaTweets){
+        try {
+            Gson gson = new GsonBuilder().setDateFormat("dd-MM-yyyy HH:mm:ss").create();
+            String json = gson.toJson(listaTweets);
+            Files.write(Paths.get(fichero), json.getBytes());
+            System.out.println("Fichero JSON escrito");
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Error al escribir en el fichero JSON");
+        }
+    }
+}
--- twitterAPI/src/main/java/dis/ufv/twitterAPI/twitterAPI/TwitterController.java
+++ twitterAPI/src/main/java/dis/ufv/twitterAPI/twitterAPI/TwitterController.java
@@ -1,11 +1,30 @@
 package dis.ufv.twitterAPI.twitterAPI;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class TwitterController {
+
+    Tweets listaTweets = new Tweets();
     @GetMapping("/tweets")
-
-
+    public ArrayList<Tweet> getTweets(){
+        return new LectorJSON().LeerJSON("./src/main/resources/tweets.json");
+    }
+    //Metodo que escribe un tweet en el fichero JSON y si no existe lo crea
+    @PostMapping("/tweet")
+    public void postTweet(@RequestBody String stringtweet) {
+        //Se añade el tweet al ArrayList convirtiendo el String en un objeto Tweet
+        Tweet tweet = new GsonBuilder().setDateFormat("dd-MM-yyyy HH:mm:ss").create().fromJson(stringtweet, Tweet.class);
+        //Se le asigna un id aleatorio al tweet de 6 digitos
+        tweet.setId((int) (Math.random() * 999999 + 100000));
+        listaTweets.addTweet(tweet);
+    }
+    @GetMapping("/tweet/{id}")
+    public Tweet getTweetById(@PathVariable int id){
+        return listaTweets.getTweetById(id);
+    }
 }
